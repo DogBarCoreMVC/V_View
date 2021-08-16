b30b908618ff6ab8ed1cf7bd8ccde50d92785c8d
GitDogBar <Dogbar>
2021-08-16 18:43:48 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using V_View.Models;
+
+namespace V_View.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]//Attribute HttpPost ทำการรับข้อมูลที่ user กรอกไว้ใน form
+        public IActionResult GetIndex(Customer customer)//ผ่าน
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Finish", customer);//ส่งข้อมูลที่เก็บอยู่ใน customer ไปแสดงผลใน Finish.csthml
+            }
+            return View(customer);//ถ้าข้อมูลไม่สมบูรณ์หรือไม่ถูกต้อง ให้แสดงผลหน้า index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace V_View.Models
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+
+        public string FullName { get; set; }
+
+        public string Address { get; set; }
+    }
+}
